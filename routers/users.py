8897a4c61b0a89155663ecf6a4fb083a7dd1a7ca
Jude Bislig <judebislig@gmail.com>
2026-04-12 18:49:38 -0400

    created skeleton for users.py to use database

--- routers/users.py
+++ routers/users.py
@@ -7,19 +7,16 @@ from schemas.user import UserCreate, UserUpdate, UserResponse
 from models.user import User
 
 
-@app.post("/users")
-def create_user(user: User):
-    users.append(user)
-    return users
+@app.post("/", response_model=UserResponse)
+def create_user(user: UserCreate, db: Session = Depends(get_db)):
+    pass
 
 
-@app.get("/users/{user_id}", response_model=User)
-def get_user(user_id: int) -> User:
-    if user_id < len(users):
-        return users[user_id]
-    else:
-        raise HTTPException(status_code=404, detail=f"User {user_id} not found")
+@app.get("/{user_id}", response_model=UserResponse)
+def get_user(user_id: int, db: Session = Depends(get_db)):
+    pass
     
 
-@app.put("/users/{user_id}")
-def update_stats():
+@app.put("/{user_id}", response_model=UserResponse)
+def update_user(user_id: int, user: UserUpdate, db: Session = Depends(get_db)):
+    pass
