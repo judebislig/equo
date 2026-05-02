9ce9341815eac51109716f52f272875dff04735c
Jude Bislig <judebislig@gmail.com>
2026-05-02 15:45:56 -0400

    created equo.js for all API calls

---
+++ equo-frontend/src/api/equo.js
@@ -0,0 +1,45 @@
+// src/api/equo.js
+const BASE_URL = "http://18.224.179.88:8000" // EC2 instance
+
+export const getToday = async (userId) => {
+    const res = await fetch(`${BASE_URL}/summary/${userId}/today`)
+    return res.json()
+}
+
+export const parseMeal = async (description) => {
+    const res = await fetch(`${BASE_URL}/meals/parse?description=${encodeURIComponent(description)}`)
+    return res.json()
+}
+
+export const logMeal = async (userId, mealType, description) => {
+    const res = await fetch(`${BASE_URL}/meals/`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ user_id: userId, meal_type: mealType, description})
+    })
+    return res.json()
+}
+
+export const logWorkout = async (userId, activityType, durationMinutes, caloriesOverride) => {
+    const res = await fetch(`${BASE_URL}/workouts/`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+            user_id: userId,
+            activity_type: activityType,
+            duration_minutes: durationMinutes,
+            calories_override: caloriesOverride || null
+        })
+    })
+    return res.json()
+}
+
+export const getTodaysMeals = async (userId) => {
+    const res = await fetch(`${BASE_URL}/meals/${userId}/today`)
+    return res.json()
+}
+
+export const getTodaysWorkouts = async (userId) => {
+    const res = await fetch(`${BASE_URL}/workouts/${userId}/today`)
+    return res.json()
+}
